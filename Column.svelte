acc1978d6fb19ef98abc31c9f591cd4c83db259d
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-07 10:01:41 +0200

    Add files via upload

---
+++ Column.svelte
@@ -0,0 +1,43 @@
+<script>
+    let {id,height,color} = $props();
+</script>
+<style>
+    .column
+    {
+        background-color: --color;
+        width: 30px;
+        border-radius: 10px;
+        margin: 0 10px;
+        box-shadow: -5px -5px lightgray;
+    }
+    .column:hover
+    {
+        transition: linear 0.5s;
+        box-shadow: -5px -5px darkgray;
+    }
+    p
+    {
+        vertical-align: middle;
+        text-align: center;
+    }
+</style>
+<section>
+    
+    {#if height >= 0}
+    <p>{height}</p>
+<div
+id={String(id)}
+class="column"
+style="height: {height}px; background-color: {color}"
+>
+</div>
+{:else}
+<div
+id={String(id)}
+class="column"
+style="height: {height * -1}px; background-color: {color}"
+>
+</div>
+<p>{height}</p>
+{/if}
+</section>
---
+++ Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    let {value = "Hello there"} = $props();
+</script>
+<style>
+    h1
+    {
+        font-weight: bold;
+        font-size: 25px;
+        text-align: center;
+    }
+</style>
+<h1>{value}</h1>
