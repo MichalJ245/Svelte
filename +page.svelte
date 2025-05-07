25c9ec438292e496aa192e200624a0183059b613
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-07 14:58:26 +0200

    Add files via upload

--- +page.svelte
+++ +page.svelte
@@ -1,4 +1,5 @@
 <script>
     import Charts from './Charts.svelte';
 </script>
-<Charts />
+<Charts slupki={[35,64,15,47,45,38,74,39]} title="Engagement" value="+46%" active_bar_id={4}/>
+<Charts slupki={[78,9,57,45,98,67,56,30]} title="Followers" value="+25%" active_bar_id={6}/>
