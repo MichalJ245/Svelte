9aae747428691b1156e3970c76f9bea1ed81cb99
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-08 11:01:16 +0200

    Add files via upload

--- +page.svelte
+++ +page.svelte
@@ -3,12 +3,11 @@
 </script>
 
 <Charts
-	bars={[35, 64, 15, 47, 45, 38, 74, 39]}
+	bars={[[35,false],[69,true],[12,true],[94,false],[49,false],[67,true],[18,true],[30,false]]}
 	title="Engagement"
 	value="+46%"
-	n_active_bar_id={[1,6]}
+    do_neg_bars={false}
     negative_bars={[]}
-    n_active_neg_bar={[]}
     active_col_pos="bg-yellow-500"
     active_col_neg="bg-red-200"
     nonactive_col="bg-gray-300"
@@ -20,12 +19,11 @@
 <br>
 <br>
 <Charts
-	bars={[78, 9, 57, 45, 98, 67, 56, 30]}
+    bars={[[98,true],[18,false],[38,false],[89,true],[34,false],[75,true],[21,true],[85,false]]}
 	title="Followers"
 	value="+25%"
-	n_active_bar_id={[5,6]}
-	negative_bars={[-30, -54, -12, -47, -68, -62, -84, -31]}
-	n_active_neg_bar={[4,7]}
+    do_neg_bars = {true}
+	negative_bars={[[15,false],[78,false],[67,true],[45,true],[99,true],[85,false],[21,true],[77,true]]}
     active_col_pos="bg-green-600"
     active_col_neg="bg-red-200"
     nonactive_col="bg-gray-300"
--- Charts.svelte
+++ Charts.svelte
@@ -1,45 +1,63 @@
 <script lang="ts">
+    type multitype = (number | boolean)[][];
 	let {
 		bars,
 		title,
 		value,
-		n_active_bar_id,
-        negative_bars = [],
-        n_active_neg_bar = [],
-        active_col_pos,
-        active_col_neg,
-        nonactive_col
-	}: { bars: number[]; title: string; value: string; n_active_bar_id: number[]; negative_bars: number[]; n_active_neg_bar: number[]; active_col_pos: string; active_col_neg: string; nonactive_col: string} = $props();
+        do_neg_bars,
+		negative_bars,
+		active_col_pos,
+		active_col_neg,
+		nonactive_col
+	}: {
+		bars: multitype;
+		title: string;
+		value: string;
+        do_neg_bars: boolean;
+		negative_bars: multitype;
+		active_col_pos: string;
+		active_col_neg: string;
+		nonactive_col: string;
+	} = $props();
 </script>
 
-<div id="main" class="max-w-max max-h-screen flex flex-col mx-auto shadow-2xl rounded-[50px] p-10 border-solid border-1 border-gray-200 outline-10 outline-white">
+<div
+	id="main"
+	class="max-w-max max-h-screen flex flex-col mx-auto shadow-2xl rounded-[50px] p-10 border-solid border-1 border-gray-200 outline-10 outline-white"
+>
 	<h1 class="font-bold text-2xl text-left">{title}</h1>
-	<p class="text-left p-1 bg-green-200 rounded-xl w-14 text-green-700 font-semibold">{value}</p>
+	<p class="text-left p-1 bg-green-100 rounded-xl w-14 text-green-700 font-semibold">{value}</p>
 	<div id="wykres" class="flex flex-col max-h-screen">
 		<div class="flex flex-row items-end max-h-screen">
-			{#each bars as bar, i}
+			{#each bars as bar}
 				<div class="flex flex-col align-middle mt-0">
-						{#if !n_active_bar_id.includes(i+1)}
-							<div class="w-3 rounded-xl mx-2 {active_col_pos}" style="height: {bar}px"></div>
-						{:else}
-							<div class="w-3 rounded-xl mx-2 {nonactive_col}" style="height: {bar}px"></div>
-						{/if}
-						<div class="w-3 mx-2 h-2"></div>
+					{#if bar[1]}
+						<div class="w-3 rounded-xl mx-2 {active_col_pos}" style="height: {bar[0]}px"></div>
+					{:else}
+						<div class="w-3 rounded-xl mx-2 {nonactive_col}" style="height: {bar[0]}px"></div>
+					{/if}
+					<div class="w-3 mx-2 h-2"></div>
 				</div>
 			{/each}
 		</div>
 		<div class="flex flex-row items-start max-h-screen">
-            {#if negative_bars.length !== 0}
-			{#each negative_bars as bar, i}
-				<div class="flex flex-col align-middle mt-0">
-					{#if !n_active_neg_bar.includes(i+1)}
-						<div class="w-3 rounded-xl mx-2 {active_col_neg} h-auto" style="height: {bar * -1}px"></div>
-					{:else}
-                        <div class="w-3 rounded-xl mx-2 {nonactive_col} h-auto" style="height: {bar * -1}px"></div>
-					{/if}
-				</div>
-			{/each}
-            {/if}
+			{#if do_neg_bars}
+				{#each negative_bars as bar}
+					<div class="flex flex-col align-middle mt-0">
+						{#if bar[1]}
+							<div
+								class="w-3 rounded-xl mx-2 {active_col_neg} h-auto"
+								style="height: {bar[0]}px"
+							></div>
+						{:else}
+							<div
+								class="w-3 rounded-xl mx-2 {nonactive_col} h-auto"
+								style="height: {bar[0]}px"
+							></div>
+						{/if}
+					</div>
+				{/each}
+			{/if}
 		</div>
 	</div>
 </div>
