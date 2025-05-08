57a039cd6a4279606d051c3f6dde182491bd3808
MichalJ245 <michaljaruga7002@gmail.com>
2025-05-08 09:43:32 +0200

    Add files via upload

--- +page.svelte
+++ +page.svelte
@@ -1,5 +1,32 @@
 <script>
-    import Charts from './Charts.svelte';
+	import Charts from './Charts.svelte';
 </script>
-<Charts slupki={[35,64,15,47,45,38,74,39]} title="Engagement" value="+46%" active_bar_id={4}/>
-<Charts slupki={[78,9,57,45,98,67,56,30]} title="Followers" value="+25%" active_bar_id={6}/>
+
+<Charts
+	bars={[35, 64, 15, 47, 45, 38, 74, 39]}
+	title="Engagement"
+	value="+46%"
+	n_active_bar_id={[1,6]}
+    negative_bars={[]}
+    n_active_neg_bar={[]}
+    active_col_pos="bg-yellow-500"
+    active_col_neg="bg-red-200"
+    nonactive_col="bg-gray-300"
+/>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<Charts
+	bars={[78, 9, 57, 45, 98, 67, 56, 30]}
+	title="Followers"
+	value="+25%"
+	n_active_bar_id={[5,6]}
+	negative_bars={[-30, -54, -12, -47, -68, -62, -84, -31]}
+	n_active_neg_bar={[4,7]}
+    active_col_pos="bg-green-600"
+    active_col_neg="bg-red-200"
+    nonactive_col="bg-gray-300"
+/>
--- Charts.svelte
+++ Charts.svelte
@@ -1,47 +1,45 @@
 <script lang="ts">
-    let {slupki,title,value,active_bar_id} : {slupki: number[],title:string,value:string,active_bar_id:number} = $props()
+	let {
+		bars,
+		title,
+		value,
+		n_active_bar_id,
+        negative_bars = [],
+        n_active_neg_bar = [],
+        active_col_pos,
+        active_col_neg,
+        nonactive_col
+	}: { bars: number[]; title: string; value: string; n_active_bar_id: number[]; negative_bars: number[]; n_active_neg_bar: number[]; active_col_pos: string; active_col_neg: string; nonactive_col: string} = $props();
 </script>
-<div id="main" class="max-w-max max-h-screen flex flex-col mx-auto">
-<h1 class="font-bold text-2xl text-left">{title}</h1>
-<p class="text-left p-2 bg-green-200 rounded-xl w-14">{value}</p>
-<div id="wykres" class="flex flex-col max-h-screen">
-<div class="flex flex-row items-end max-h-screen">
-{#each slupki as slupek,i}
-    <div class="flex flex-col align-middle mt-0">
-        {#if slupek > 0}
-        {#if active_bar_id == i}
-        <div
-        class="w-2 rounded-xl mx-2 bg-yellow-500"
-        style="height: {slupek}px"
-        >
-        </div>
-        {:else}
-        <div
-        class="w-2 rounded-xl mx-2 bg-gray-500"
-        style="height: {slupek}px"
-        >
-        </div>
-        {/if}
-        {:else}
-        <div class="w-2 mx-2 h-2"></div>
-        {/if}
-    </div>
-{/each}
-</div>
-<div class="flex flex-row items-start max-h-screen">
-    {#each slupki as slupek}
-    <div class="flex flex-col align-middle mt-0">
-        {#if slupek < 0}
-        <div
-        class="w-2 rounded-xl mx-2 bg-gray-500"
-        style="height: {slupek*-1}px"
-        >
-        </div>
-        {:else}
-        <div class="w-2 mx-2 h-2"></div>
-        {/if}
-    </div>
-    {/each}
-</div>
-</div>
+
+<div id="main" class="max-w-max max-h-screen flex flex-col mx-auto shadow-2xl rounded-[50px] p-10 border-solid border-1 border-gray-200 outline-10 outline-white">
+	<h1 class="font-bold text-2xl text-left">{title}</h1>
+	<p class="text-left p-1 bg-green-200 rounded-xl w-14 text-green-700 font-semibold">{value}</p>
+	<div id="wykres" class="flex flex-col max-h-screen">
+		<div class="flex flex-row items-end max-h-screen">
+			{#each bars as bar, i}
+				<div class="flex flex-col align-middle mt-0">
+						{#if !n_active_bar_id.includes(i+1)}
+							<div class="w-3 rounded-xl mx-2 {active_col_pos}" style="height: {bar}px"></div>
+						{:else}
+							<div class="w-3 rounded-xl mx-2 {nonactive_col}" style="height: {bar}px"></div>
+						{/if}
+						<div class="w-3 mx-2 h-2"></div>
+				</div>
+			{/each}
+		</div>
+		<div class="flex flex-row items-start max-h-screen">
+            {#if negative_bars.length !== 0}
+			{#each negative_bars as bar, i}
+				<div class="flex flex-col align-middle mt-0">
+					{#if !n_active_neg_bar.includes(i+1)}
+						<div class="w-3 rounded-xl mx-2 {active_col_neg} h-auto" style="height: {bar * -1}px"></div>
+					{:else}
+                        <div class="w-3 rounded-xl mx-2 {nonactive_col} h-auto" style="height: {bar * -1}px"></div>
+					{/if}
+				</div>
+			{/each}
+            {/if}
+		</div>
+	</div>
 </div>
